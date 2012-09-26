d469dad08b4624396f7ce4b51bd8ed05916b3984
Steven Weller <github@suweller.com>
2012-09-26 15:21:47 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideas::Application.config.secret_token = '61e65ac922d388875b5344ec6784cbb2ae94f781aafa3844eea118504564a29d65e5e3cefc1765ca52def880c15fd5de082b2c02b0ce9fcdaa08c6b53c0fe1bf'
